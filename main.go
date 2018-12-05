63e7f58269e453679fb1783ca611bf17a7338707
shengbo66 <45621248+shengbo66@users.noreply.github.com>
2018-12-05 17:10:13 +0800

    Update main.go
    update the output in the screen, minor udpate

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello Shengbo, you are great! - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
